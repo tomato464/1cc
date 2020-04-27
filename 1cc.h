5082163ac1385114224393d372332de638aceac8
tomto464 <kajita.shun@b.mbox.nagoya-u.ac.jp>
2020-04-27 17:36:04 +0900

    ファイルを分割した

---
+++ 1cc.h
@@ -0,0 +1,98 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//token.c
+typedef enum
+{
+	TK_RESERVED,	//記号
+	TK_NUM,		//整数トークン
+	TK_EOF,		//入力の終わりを表すトークン
+}Tokenkind;
+
+typedef struct Token Token;
+
+//トークン型
+struct Token
+{
+	Tokenkind kind;	//トークンの型
+	Token *next;	//次の入力トークン
+	int val;	//kindがTK_NUMの場合、その値
+	char *str;	//トークン文字列
+	int len;	//トークン文字列の長さ
+};
+
+//入力された文字列
+char *user_input;
+
+//現在着目しているトークン
+Token *token;
+
+//エラーを報告するための関数
+//printfと同じ引数を取る
+void error(char *fmt, ...);
+
+void error_at(char *loc, char *fmt, ...);
+
+//次のトークンが期待している記号の時には、トークンを一つ進めて
+//真を返す。それ以外の時には偽を返す
+bool consume(char *op);
+
+//次のトークンが数値の場合、トークンを一つ進めてその数値を返す
+//それ以外の場合にはエラーを報告する
+int expect_number();
+
+bool at_eof();
+
+//新しいトークンを生成してcurに繋げる
+Token *new_token(Tokenkind kind, Token *cur, char *str, int len);
+
+bool startswith(char *p, char *q);
+
+//入力文字列pをトークナイズしてそれを返す
+Token *tokenize();
+
+//parser.c
+typedef enum{
+	ND_ADD,	// +
+	ND_SUB,	// -
+	ND_MUL,	// *
+	ND_DIV,	// /
+	ND_EQ,	// ==
+	ND_NE,	// !=
+	ND_LT,	// <
+	ND_LE,	// <=
+	ND_NUM,	// integer
+} Nodekind;
+
+typedef struct Node Node;
+
+struct Node
+{
+	Nodekind kind;	//ノードの種類
+	Node *lhs;	//左辺
+	Node *rhs;	//右辺
+	int val;	//ND_NUMの時のみ使う
+};
+
+Node *new_node(Nodekind kind);
+
+Node *new_binary(Nodekind kind, Node *lhs, Node *rhs);
+
+Node *new_num(int val);
+
+//最初は+,-の処理を構文木を使って処理できるようにする
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+
+//codegne.c
+//コードを生成
+void gen(Node *node);
---
+++ parse.c
@@ -0,0 +1,132 @@
+#include "1cc.h"
+Node *new_node(Nodekind kind)
+{
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	return node;
+}
+
+Node *new_binary(Nodekind kind, Node *lhs, Node *rhs)
+{
+	Node *node = new_node(kind);
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_num(int val)
+{
+	Node *node = new_node(ND_NUM);
+	node->val = val;
+	return node;
+}
+
+Node *expr()
+{
+	return equality();
+}
+
+//equality = relational ("=="relational | "!="relational)*
+Node *equality()
+{
+	Node *node = relational();
+	
+	for(;;){
+		if(consume("==")){
+			node = new_binary(ND_EQ, node, relational());
+		}
+		else if(consume("!=")){
+			node = new_binary(ND_NE, node, relational());
+		}
+		else{
+			return node;
+		}
+	}
+}
+
+//relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+Node *relational()
+{
+	Node *node = add();
+
+	for(;;){
+		if(consume("<")){
+			node = new_binary(ND_LT, node, add());
+		}
+		else if(consume(">")){
+			node = new_binary(ND_LT, add(), node);
+		}
+		else if(consume("<=")){
+			node = new_binary(ND_LE, node, add());
+		}
+		else if(consume(">=")){
+			node = new_binary(ND_LE, add(), node);
+		}
+		else{
+			return node;
+		}
+	}
+}
+
+// add = mul ("+" mul | "-"mul)*
+Node *add()
+{
+	Node *node = mul();
+	
+	for(;;){
+		if(consume("+")){
+			node = new_binary(ND_ADD, node, mul());
+		}
+		else if(consume("-")){
+			node = new_binary(ND_SUB, node, mul());
+		}
+		else{
+			return node;
+		}
+	}
+}
+
+//mul = unary ("*"unary | "/"unary)*
+Node *mul()
+{
+	Node *node = unary();
+
+	for(;;){
+		if(consume("*")){
+			node = new_binary(ND_MUL, node, unary());
+		}
+		else if(consume("/")){
+			node = new_binary(ND_DIV, node, unary());
+		}
+		else{
+			return node;
+		}
+	}
+}
+
+//unary = ('+' | '-')?primary
+Node *unary()
+{
+	if(consume("+")){
+		return primary();
+	}
+	else if(consume("-")){
+		return new_binary(ND_SUB, new_num(0), primary()); 
+	}
+	else{
+		return primary();
+	}
+	
+}
+
+//primary = num | "("expr")"
+Node *primary()
+{
+	if(consume("(")){
+		Node *node = expr();
+		consume(")");
+		return node;
+	}
+
+	return new_num(expect_number());
+}
