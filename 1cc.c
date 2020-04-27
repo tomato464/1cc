761386e899107310074b725ff628e3b604b837d4
tomto464 <kajita.shun@b.mbox.nagoya-u.ac.jp>
2020-04-27 11:59:50 +0900

    トークンにlen属性を追加¼

--- 1cc.c
+++ 1cc.c
@@ -23,6 +23,7 @@ struct Token
 	Token *next;	//次の入力トークン
 	int val;	//kindがTK_NUMの場合、その値
 	char *str;	//トークン文字列
+	int len;	//トークン文字列の長さ
 };
 
 //入力された文字列
@@ -59,9 +60,10 @@ void error_at(char *loc, char *fmt, ...)
 
 //次のトークンが期待している記号の時には、トークンを一つ進めて
 //真を返す。それ以外の時には偽を返す
-bool consume(char op)
+bool consume(char *op)
 {
-	if(token->kind != TK_RESERVED || token->str[0] != op){
+	if(token->kind != TK_RESERVED || strlen(op) != token->len ||
+		memcmp(token->str, op, token->len)){
 		return false;
 	}
 	token = token->next;
@@ -70,9 +72,10 @@ bool consume(char op)
 
 //次のトークンが期待している記号の時には、トークンを一つ読み進める。
 //それ以外の場合にはエラーを報告する。
-void expect(char op)
+void expect(char *op)
 {
-	if(token->kind != TK_RESERVED || token->str[0] != op){
+	if(token->kind != TK_RESERVED || strlen(op) != token->len ||
+		memcmp(token->str, op, token->len)){
 		error_at(token->str, "'%c'ではありません", op);
 	}
 	token = token->next;
@@ -95,13 +98,19 @@ bool at_eof()
 }
 
 //新しいトークンを生成してcurに繋げる
-Token *new_token(Tokenkind kind, Token *cur, char *str)
+Token *new_token(Tokenkind kind, Token *cur, char *str, int len)
 {
 	Token *tok = calloc(1, sizeof(Token));
 	tok->kind = kind;
 	tok->str = str;
+	tok->len = len;
 	cur->next = tok;
 	return tok;
+}
+
+bool startswith(char *p, char *q)
+{
+	return memcmp(p, q, strlen(q)) == 0;//p==qでtrueを返す
 }
 
 //入力文字列pをトークナイズしてそれを返す
@@ -118,22 +127,30 @@ Token *tokenize()
 			p++;
 			continue;
 		}
+		//複数文字
+		if(startswith(p, "==") || startswith(p, "!=") ||
+			startswith(p, ">=") || startswith(p, "<=")){
+			cur = new_token(TK_RESERVED, cur, p, 2);
+			p += 2;
+		}
 
-		if(strchr("+-*/()", *p)){
-			cur = new_token(TK_RESERVED, cur, p);
+		if(strchr("+-*/()<>", *p)){
+			cur = new_token(TK_RESERVED, cur, p, 1);
 			p++;
 			continue;
 		}
 
 		if(isdigit(*p)){
-			cur = new_token(TK_NUM, cur, p);
-			cur->val = strtol(p, &p, 10);
+			cur = new_token(TK_NUM, cur, p, 0);//とりあえず0を入れておく
+			char *q = p;
+			cur->val = strtol(p, &p, 10);//pの値を更新
+			cur->len = p - q;
 			continue;
 		}
 
 		error_at(p, "数でない");
 	}
-	new_token(TK_EOF, cur, p);
+	new_token(TK_EOF, cur, p, 0);
 	return head.next;
 }
 
@@ -189,10 +206,10 @@ Node *expr()
 	Node *node = mul();
 	
 	for(;;){
-		if(consume('+')){
+		if(consume("+")){
 			node = new_binary(ND_ADD, node, mul());
 		}
-		else if(consume('-')){
+		else if(consume("-")){
 			node = new_binary(ND_SUB, node, mul());
 		}
 		else{
@@ -207,10 +224,10 @@ Node *mul()
 	Node *node = unary();
 
 	for(;;){
-		if(consume('*')){
+		if(consume("*")){
 			node = new_binary(ND_MUL, node, unary());
 		}
-		else if(consume('/')){
+		else if(consume("/")){
 			node = new_binary(ND_DIV, node, unary());
 		}
 		else{
@@ -222,10 +239,10 @@ Node *mul()
 //unary = ('+' | '-')?primary
 Node *unary()
 {
-	if(consume('+')){
+	if(consume("+")){
 		return primary();
 	}
-	else if(consume('-')){
+	else if(consume("-")){
 		return new_binary(ND_SUB, new_num(0), primary()); 
 	}
 	else{
@@ -237,9 +254,9 @@ Node *unary()
 //primary = num | "("expr")"
 Node *primary()
 {
-	if(consume('(')){
+	if(consume("(")){
 		Node *node = expr();
-		consume(')');
+		consume(")");
 		return node;
 	}
 
