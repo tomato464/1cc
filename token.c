f936f66626e2d29a6f65265e76844a1064367be1
tomto464 <kajita.shun@b.mbox.nagoya-u.ac.jp>
2020-06-15 19:17:58 +0900

    add sizeof operation

--- token.c
+++ token.c
@@ -101,7 +101,7 @@ static bool startswith(char *p, char *q)
 
 static bool is_keyword(Token *tok)
 {
-	char *key[] = {"while", "if", "for", "return", "else", "int"};
+	char *key[] = {"while", "if", "for", "return", "else", "int", "sizeof"};
 
 	for(int i = 0; i < sizeof(key) / sizeof(*key); i++){
 		if(equal(tok, key[i])){
